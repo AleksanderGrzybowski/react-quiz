0725ac9ceae459855c495e905f56bca3b1670c57
Aleksander Grzybowski <kelog@orange.pl>
2017-05-12 16:40:55 +0200

    Progress bar on summary view

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Col, Row } from 'reactstrap';
+import { Col, Progress, Row } from 'reactstrap';
+
+const ProgressBar = ({score, total}) => {
+    const progressValue = 100 * score / total;
+    let color;
+    if (progressValue < 20) {
+        color = 'danger'
+    } else if (progressValue < 60) {
+        color = 'info';
+    } else {
+        color = 'success';
+    }
+
+    return <Progress color={color} value={progressValue}/>
+};
 
 const Summary = ({score, total}) => (
-    <div>
-        <Row>
-            <Col md={12}>
-                <h1 className="text-center">
-                    You scored {score} / {total}
-                </h1>
-            </Col>
-        </Row>
-    </div>
+    <Row>
+        <Col md={12}>
+            <h1 className="text-center">
+                You scored {score} / {total}
+                <ProgressBar score={score} total={total}/>
+            </h1>
+        </Col>
+    </Row>
 );
 
 export default Summary;
